b5395b9b648fb811ffc03636de7d591cc8d9d519
Amir Mustafa <amirengg15@gmail.com>
2021-12-19 16:49:00 +0530

    feat: basic lambda code

--- index.ts
+++ index.ts
@@ -1,10 +1,12 @@
     import { APIGatewayProxyEvent, APIGatewayProxyResultV2, Handler } from "aws-lambda";
+    import * as _ from "lodash";
     
     export const handler:Handler = async(event: APIGatewayProxyEvent): Promise<APIGatewayProxyResultV2> => {
-        // TODO implement
+        const max = 999;
+        const val = _.random(max);
         const response = {
             statusCode: 200,
-            body: JSON.stringify('Hello from Lambda using CI/CD pipeline!'),
+            body: `The random value is (max ${max}) is: ${val}`,
         };
         return response;
     }
